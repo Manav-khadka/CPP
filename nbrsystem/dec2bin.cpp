25980bebff238a779dce07783160402bb1c005fe
Manav Khadka <manavkhadka2004@gmail.com>
2022-04-02 00:32:10 +0530

    dec2bin

---
+++ nbrsystem/dec2bin.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+long long  dec2bin(int);
+
+int main()
+
+{
+    int dec;
+    cout<<"enter decimal number:";
+    cin>>dec;
+    cout<<"the binary of given decimal number is:  "<<dec2bin(dec)<<endl;
+    
+    return 0;
+
+}
+long long  dec2bin(int dec)
+{
+    long long bin=0;
+    int i=1,rem;
+    while (dec!=0)
+    {
+        rem=dec%2;
+        dec/=2;
+        bin+=rem*i;
+        i*=10;
+
+    }
+    return bin;
+}
