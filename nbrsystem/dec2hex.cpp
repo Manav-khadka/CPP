702d982769ace3bce730978ba12d405e08456902
Manav Khadka <manavkhadka2004@gmail.com>
2022-04-02 23:55:43 +0530

    todays last commit

---
+++ nbrsystem/dec2hex.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+//convert decimal to hexadecimal
+void convert(int num) {
+   char arr[100];
+   int i = 0;
+   while(num!=0) {
+      int temp = 0;
+      temp = num % 16;
+      if(temp < 10) {
+         arr[i] = temp + 48;
+         i++;
+      } else {
+         arr[i] = temp + 55;
+         i++;
+      }
+      num = num/16;
+   }
+   for(int j=i-1; j>=0; j--)
+   cout << arr[j];
+}
+int main() {
+   int num;
+   cout<<"enter decimal digit";
+   cin>>num;
+   cout<<num<< " converted to hexadeciaml: ";
+   convert(num);
+   return 0;
+}
