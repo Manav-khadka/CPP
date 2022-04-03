46147ceb82092c5ae561eff782d80570aa65e63b
Manav Khadka <manavkhadka2004@gmail.com>
2022-04-03 23:36:39 +0530

    strong number

---
+++ basics/strongnumber.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    int fact=1;
+    for (int i = 1; i <=n; i++)
+    {
+       fact=fact*i;
+
+    }
+    return fact;
+    
+}
+int strongnbr(int num){
+    int digit,sum=0;
+    int temp=num;//sum has same value remained the value of temp will change
+    while (temp!=0)
+    {
+        digit=temp%10;//we  have to find every single digit's fatorial
+        sum=sum+factorial(digit);
+        temp/=10;
+
+
+    }
+    return sum == num;
+    
+
+}
+int main()
+
+{
+    int num;
+    cin>>num;
+    if (strongnbr(num))
+    
+        cout<<"strong number";
+    
+    else
+    
+        cout<<"non strong number";
+    
+    
+    
+    return 0;
+
+}
