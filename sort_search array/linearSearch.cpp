98210e8fc850488071e608f19df507d0b747c77b
Manav Khadka <manavkhadka2004@gmail.com>
2022-04-08 13:42:30 +0530

    linear search in array

---
+++ sort_search array/linearSearch.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int linearSearch(int arr[], int n, int key )
+{
+    for (int  i = 0; i <n; i++)
+    {
+        if (arr[i]==key)
+        {
+            return i;
+        }
+        
+    }
+        return -1;
+    
+}
+
+int main()
+
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for (int i = 0; i <n; i++)
+    {
+        cin>>a[i];
+    }
+    int key;
+    cin>>key;
+    cout<<linearSearch(a,n,key)<<"\n";
+    
+
+}
