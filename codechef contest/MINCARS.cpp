9111e23ff549b2bde414ba1134a57d98ff5ab359
Manav Khadka <manavkhadka2004@gmail.com>
2022-04-12 22:29:36 +0530

    commit

---
+++ codechef contest/MINCARS.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    double n;
+	    double res;
+	    cin>>n;
+	    res = ceil(n/4);
+	    cout<<res<<endl;
+
+	}
+	return 0;
+}
